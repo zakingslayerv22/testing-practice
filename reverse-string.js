b8f19714292f80686a2720d70511b0a44249b3e8
Eriwe-li <11926638+zakingslayerv22@users.noreply.github.com>
2025-04-14 07:58:03 -0400

    Refactor reverseString

--- reverse-string.js
+++ reverse-string.js
@@ -1,10 +1,3 @@
 export function reverseString(string) {
-  const splitString = string.split("");
-
-  let text = "";
-  for (let i = splitString.length - 1; i >= 0; i--) {
-    text = text + splitString[i];
-  }
-
-  return text;
+  return string.split("").reverse().join("");
 }
